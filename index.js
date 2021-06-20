b15525e00566615f1f696588f12293c762fed4bc
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-06-19 20:54:05 -0400

    built out questions and added basic JS handling for the response

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What should this project be called?',
+        name: 'Project Title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description for the project:',
+        name: 'Description',
+    },
+    {
+        type: 'input',
+        message: 'Specify installation instructions:',
+        name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: `What is the project's purpose/usage?`,
+        name: 'Usage',
+    },
+    {
+        type: 'input',
+        message: 'How best can others contribute to the project?',
+        name: 'Contributing',
+    },
+    {
+        type: 'input',
+        message: 'Specify testing instructions:',
+        name: 'Tests',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}`, data, 'utf8', (error, response) =>
+    error ? console.error(error) : console.log(JSON.stringify(response)))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        writeToFile(`README.md`, response);
+    });
+}
 
 // Function call to initialize app
 init();
